db382f81fa408c1971a1e0347d25b7e2288bd25c
李俊辉 <3424837182@qq.com>
2025-11-27 20:46:07 +0800

    测试
    测试

---
+++ cyuyan.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10, t1 = 0, t2 = 1, next;
+    printf("Fibonacci Series: %d %d ", t1, t2);
+    for (int i = 3; i <= n; ++i) {
+        next = t1 + t2;
+        printf("%d ", next);
+        t1 = t2;
+        t2 = next;
+    }
+    printf("\n");
+    return 0;
+}
